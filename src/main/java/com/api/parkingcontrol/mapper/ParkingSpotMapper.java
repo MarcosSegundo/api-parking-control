58294c133966b6a2ee6e5c3153f0ef266c8dab96
marcos segundo <marcos.segundo.cz@gmail.com>
2022-02-17 20:06:09 -0300

    removing annotation mapping for datetime field

--- src/main/java/com/api/parkingcontrol/mapper/ParkingSpotMapper.java
+++ src/main/java/com/api/parkingcontrol/mapper/ParkingSpotMapper.java
@@ -3,7 +3,6 @@ package com.api.parkingcontrol.mapper;
 import com.api.parkingcontrol.dto.ParkingSpotDTO;
 import com.api.parkingcontrol.entities.ParkingSpot;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
@@ -11,9 +10,6 @@ public interface ParkingSpotMapper {
 
     ParkingSpotMapper INSTANCE = Mappers.getMapper(ParkingSpotMapper.class);
 
-    @Mapping(target = "registrationDate", source = "registrationDate", dateFormat = "yyyy-MM-dd HH:mm")
     ParkingSpot toModel(ParkingSpotDTO parkingSpotDTO);
-
-    @Mapping(target = "registrationDate", source = "registrationDate", dateFormat = "yyyy-MM-dd HH:mm")
     ParkingSpotDTO toDTO(ParkingSpot parkingSpot);
 }
