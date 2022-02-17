958c5c88f5f0c3556a50915c3ef694404d2167d2
marcos segundo <marcos.segundo.cz@gmail.com>
2022-02-16 21:37:38 -0300

    created entity classes, ParkingSpont and Vehicle

--- src/main/java/com/api/parkingcontrol/entities/ParkingSpot.java
+++ src/main/java/com/api/parkingcontrol/entities/ParkingSpot.java
@@ -1,2 +1,54 @@
-package com.api.parkingcontrol.entities;public class ParkingSpot {
+package com.api.parkingcontrol.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_parking_spot")
+public class ParkingSpot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 10)
+    private String parkingSpotNumber;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "vehicle_id", referencedColumnName = "id")
+    private Vehicle vehicle;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDateTime registrationDate;
+
+    @Column(nullable = false, length = 130)
+    private String responsibleName;
+
+    @Column(nullable = false, length = 30)
+    private String apartment;
+
+    @Column(nullable = false, length = 30)
+    private String block;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ParkingSpot that = (ParkingSpot) o;
+        return id.equals(that.id) && parkingSpotNumber.equals(that.parkingSpotNumber) && vehicle.equals(that.vehicle) && registrationDate.equals(that.registrationDate) && responsibleName.equals(that.responsibleName) && apartment.equals(that.apartment) && block.equals(that.block);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, parkingSpotNumber, vehicle, registrationDate, responsibleName, apartment, block);
+    }
 }
--- src/main/java/com/api/parkingcontrol/entities/Vehicle.java
+++ src/main/java/com/api/parkingcontrol/entities/Vehicle.java
@@ -1,19 +1,46 @@
 package com.api.parkingcontrol.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.persistence.*;
+import java.util.Objects;
 import java.util.UUID;
 
-@Data
+@Setter
+@Getter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class Car {
+@Entity
+@Table(name = "tb_vehicle")
+public class Vehicle {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
+    @Column(nullable = false, unique = true, length = 7)
     private String plate;
+
+    @Column(nullable = false, length = 70)
     private String brand;
+
+    @Column(nullable = false, length = 70)
     private String model;
+
+    @Column(nullable = false, length = 70)
     private String color;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vehicle vehicle = (Vehicle) o;
+        return id.equals(vehicle.id) && plate.equals(vehicle.plate) && brand.equals(vehicle.brand) && model.equals(vehicle.model) && color.equals(vehicle.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, plate, brand, model, color);
+    }
 }
