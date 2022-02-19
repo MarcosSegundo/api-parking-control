615acfaed88253ebcf953521b0215daa4ed2d641
marcos segundo <marcos.segundo.cz@gmail.com>
2022-02-18 21:59:37 -0300

    developed a controller advice to handle a custom validation exceptions

--- src/main/java/com/api/parkingcontrol/handlers/ApiHandlerException.java
+++ src/main/java/com/api/parkingcontrol/handlers/ApiHandlerException.java
@@ -1,2 +1,30 @@
-package com.api.parkingcontrol.handlers;public class ApiHandlerException {
+package com.api.parkingcontrol.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ApiHandlerException {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errs = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String field = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errs.put(field, message);
+        });
+
+        return new ResponseEntity<>(errs, HttpStatus.BAD_REQUEST);
+    }
 }
