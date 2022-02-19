6138a4ef1d6b570e36af07c88b58bf0b2101d16f
marcos segundo <marcos.segundo.cz@gmail.com>
2022-02-18 21:56:44 -0300

    created a service from Vehicle class

--- src/main/java/com/api/parkingcontrol/services/VehicleService.java
+++ src/main/java/com/api/parkingcontrol/services/VehicleService.java
@@ -1,2 +1,20 @@
-package com.api.parkingcontrol.services;public class VehicleService {
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.repositories.VehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VehicleService {
+    
+    private final VehicleRepository vehicleRepository;
+
+    @Autowired
+    public VehicleService(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+    
+    public boolean existsByPlate(String plate) {
+        return vehicleRepository.existsByPlate(plate);
+    }
 }
