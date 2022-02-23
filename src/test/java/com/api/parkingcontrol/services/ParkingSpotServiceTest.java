6cfb949c3f08f272dccc4a9737ee581be24079aa
marcos segundo <marcos.segundo.cz@gmail.com>
2022-02-23 13:18:45 -0300

    developed unit test for findAll method.

--- src/test/java/com/api/parkingcontrol/services/ParkingSpotServiceTest.java
+++ src/test/java/com/api/parkingcontrol/services/ParkingSpotServiceTest.java
@@ -3,6 +3,7 @@ package com.api.parkingcontrol.services;
 import com.api.parkingcontrol.builder.ParkingSpotDtoBuilder;
 import com.api.parkingcontrol.dto.ParkingSpotDTO;
 import com.api.parkingcontrol.entities.ParkingSpot;
+import com.api.parkingcontrol.exceptions.ParkingSpotNotFoundException;
 import com.api.parkingcontrol.mapper.ParkingSpotMapper;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
 import org.junit.jupiter.api.Test;
@@ -10,8 +11,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -71,4 +81,33 @@ public class ParkingSpotServiceTest {
 
         assertThat(savedParkingSpotNumber).isTrue();
     }
+
+    @Test
+    public void whenFindAllIsCalledThenReturnAEmptyListOfParkingSpot() {
+        //when
+        when(parkingSpotRepository.findAll()).thenReturn(Collections.emptyList());
+
+        //then
+        List<ParkingSpot> foundedParkingSpots = parkingSpotService.findAll();
+
+        assertThat(foundedParkingSpots).isEmpty();
+    }
+
+    @Test
+    public void whenFindAllIsCalledThenReturnAListOfParkingSpot() {
+        //given
+        ParkingSpotDTO expectedParkingSpotDTO = ParkingSpotDtoBuilder.builder()
+                .build().toParkingSpotDTO();
+
+        ParkingSpot expectedParkingSpot = MAPPER.toModel(expectedParkingSpotDTO);
+
+        //when
+        when(parkingSpotRepository.findAll()).thenReturn(Collections.singletonList(expectedParkingSpot));
+
+        //then
+        List<ParkingSpot> foundedParkingSpots = parkingSpotService.findAll();
+
+        assertThat(foundedParkingSpots).hasSize(1);
+    }
+
 }
