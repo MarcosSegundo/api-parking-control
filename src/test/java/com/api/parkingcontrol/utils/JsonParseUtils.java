62bad273d6b2767e687133846c79cdbd096575e1
marcos segundo <marcos.segundo.cz@gmail.com>
2022-02-26 14:27:11 -0300

    created a Json parsed for using in ParkingSpotControllerTest.

--- src/test/java/com/api/parkingcontrol/utils/JsonParseUtils.java
+++ src/test/java/com/api/parkingcontrol/utils/JsonParseUtils.java
@@ -1,2 +1,20 @@
-package com.api.parkingcontrol.utils;public class JsonParseUtils {
+package com.api.parkingcontrol.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonParseUtils {
+
+    public static <T> String asJsonString(T dto) {
+
+        ObjectMapper mapper = new ObjectMapper();
+        String result = "";
+
+        try {
+            result = mapper.writeValueAsString(dto);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
