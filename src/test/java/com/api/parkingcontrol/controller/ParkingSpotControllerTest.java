5501465cee4c66233b681cc2982fa2832477df4b
marcos segundo <marcos.segundo.cz@gmail.com>
2022-02-26 14:28:20 -0300

    developed a integration test for POST method of ParkingSpotControllerTest.

--- src/test/java/com/api/parkingcontrol/controller/ParkingSpotControllerTest.java
+++ src/test/java/com/api/parkingcontrol/controller/ParkingSpotControllerTest.java
@@ -1,2 +1,81 @@
-package com.api.parkingcontrol.controller;public class ParkingSpotControllerTest {
+package com.api.parkingcontrol.controller;
+
+import com.api.parkingcontrol.builder.ParkingSpotDtoBuilder;
+import com.api.parkingcontrol.controllers.ParkingSpotController;
+import com.api.parkingcontrol.dto.ParkingSpotDTO;
+import com.api.parkingcontrol.entities.ParkingSpot;
+import com.api.parkingcontrol.exceptions.ParkingSpotNotFoundException;
+import com.api.parkingcontrol.mapper.ParkingSpotMapper;
+import com.api.parkingcontrol.services.ParkingSpotService;
+import com.api.parkingcontrol.services.VehicleService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.api.parkingcontrol.utils.JsonParseUtils.asJsonString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ParkingSpotController.class)
+public class ParkingSpotControllerTest {
+
+    private static final String PARKING_SPOT_API_PATH = "/api/v1/parking-spot";
+    private static final String SUB_PATH_FIND_ALL_PAGE = "/page";
+
+    @MockBean
+    private ParkingSpotService parkingSpotService;
+
+    @MockBean
+    private VehicleService vehicleService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final ParkingSpotMapper MAPPER = ParkingSpotMapper.INSTANCE;
+
+    @Test
+    public void whenPOSTIsCalledThenAParkingSpotIsCreated() throws Exception {
+        //given
+        ParkingSpotDTO parkingSpotDTO = ParkingSpotDtoBuilder.builder()
+                .build().toParkingSpotDTO();
+
+        ParkingSpot parkingSpot = MAPPER.toModel(parkingSpotDTO);
+
+        //when
+        when(parkingSpotService.save(parkingSpotDTO)).thenReturn(parkingSpot);
+
+        //then
+        mockMvc.perform(post(PARKING_SPOT_API_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(parkingSpot))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(parkingSpotDTO.getId().toString()));
+    }
+
+    @Test
+    public void whenPOSTIsCalledWithoutARequiredFieldThenAExceptionShouldThrow() throws Exception {
+        //given
+        ParkingSpotDTO parkingSpotDTO = ParkingSpotDtoBuilder.builder()
+                .build().toParkingSpotDTO();
+
+        parkingSpotDTO.setParkingSpotNumber(null);
+
+        ParkingSpot parkingSpot = MAPPER.toModel(parkingSpotDTO);
+
+        //when
+        when(parkingSpotService.save(parkingSpotDTO)).thenThrow(ParkingSpotNotFoundException.class);
+
+        //then
+        mockMvc.perform(post(PARKING_SPOT_API_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(parkingSpot))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
 }
